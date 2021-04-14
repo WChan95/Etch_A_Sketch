279a6265b20acfafc33fe3283d9f4566a1de8d37
Soejun <wc.wilsonch@gmail.com>
2021-04-13 18:04:09 -0700

    Added individual div coloring

--- sketch.js
+++ sketch.js
@@ -1,10 +1,19 @@
 
+
 const body = document.getElementById("container")
+
 
 for(i = 0; i<1600; i++){
     const div = document.createElement("div");
     div.classList.add("box");
-    div.classList.add("box:hover");
-    body.append(div);
-
+    div.setAttribute("id","etch");
+    body.appendChild(div);
 }
+
+//Allows each div to be individually colored for mouse hover
+const arrayDiv = document.querySelectorAll("#etch");
+arrayDiv.forEach(element => {
+    element.addEventListener("mouseover", (e) =>{
+        element.classList.add("colorBlack");
+    });
+});
