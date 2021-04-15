4d6b9e0e812c7e45395f600bd47c24e3a0c494de
Soejun <wc.wilsonch@gmail.com>
2021-04-14 23:14:27 -0700

    Added etch-a-sketch sizing

--- sketch.js
+++ sketch.js
@@ -1,24 +1,19 @@
 
 
-const body = document.getElementById("container")
+const body = document.getElementById("container3")
 
-//Creates size of etch a sketch
+//Creates default etch and sketch
 for(i = 0; i<16; i++){
     const div = document.createElement("div");
     div.classList.add("box");
     div.setAttribute("id","etch");
     body.appendChild(div);
+    etch();
 }
 
-//Allows each div to be individually colored for mouse hover
-const arrayDiv = document.querySelectorAll("#etch");
-arrayDiv.forEach(element => {
-    element.addEventListener("mouseover", (e) =>{
-        element.classList.add("colorBlack");
-    });
-});
 
 //Reset etch a sketh to blank slate
+const arrayDiv = document.querySelectorAll("#etch");
 const resetSketch = document.getElementById("resetSketch");
 resetSketch.addEventListener('click', (e) => {
     arrayDiv.forEach(element => {
@@ -26,7 +21,29 @@ resetSketch.addEventListener('click', (e) => {
     });
 })
 
-//Set grid and square size
-const container3 = document.getElementById("container");
-console.log(container3);
-container3.style.gridTemplateColumns = `repeat(10, 20px)`;
+const sketchForm = document.getElementById("sketchForm");
+sketchForm.addEventListener('click', (e)=>{
+    let gridSize = prompt("Please enter a number");
+    let totalBox = gridSize * gridSize;
+    body.style.gridTemplateColumns = `repeat(${gridSize}, 20px)`
+    body.style.gridTemplateRows= `repeat(${gridSize}, 20px)`
+    for(i = 0; i < totalBox; i++){
+        const div = document.createElement("div");
+        div.classList.add("box");
+        div.setAttribute("id","etch");
+        body.appendChild(div);
+    }
+    etch();
+})
+
+//Allows each div to be individually colored for mouse hover
+
+//Allows each box to be "etched"
+function etch(){
+    const arrayDiv = document.querySelectorAll("#etch");
+    arrayDiv.forEach(element => {
+        element.addEventListener("mouseover", (e) =>{
+            element.classList.add("colorBlack");
+        });
+    });
+}
